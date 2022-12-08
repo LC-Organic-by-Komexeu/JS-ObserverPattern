6f609a740c28457058f6fc775779fcfe99e84b27
Ray Huang <rayhuang@linkchain.tw>
2022-12-08 22:47:28 +0800

    feat: Transform function support

--- index.js
+++ index.js
@@ -8,18 +8,29 @@ window.LC.StoreProxy = new Proxy(window.LC.Store, {
     set(target, key, value, receiver) {
         const result = Reflect.set(target, key, value, receiver)
         Array.from(window.LC.StoreObserve).filter(a => a.key === key).forEach(observe => {
-            observe.element.value = value
+            observe.element.value = observe.transform(value)
         })
         return result
     },
 })
 
+window.LC.StoreTransform = {
+    ToUpper: (text) => {
+        return text.toUpperCase()
+    },
+    ToLower: (text) => {
+        return text.toLowerCase()
+    }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll(`[data-bind]`).forEach(e => {
         let key = e.dataset.bind
+        let transform = e.dataset.transform
         window.LC.StoreObserve.add({
             key: key,
             element: e,
+            transform: window.LC.StoreTransform[transform]
         })
     })
 });
