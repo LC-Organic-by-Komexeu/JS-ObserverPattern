674b8c9cb5b03a283d91d8c8f09e9a8652110496
Ray Huang <rayhuang@linkchain.tw>
2022-12-08 22:40:29 +0800

    feat: One-Way Data Binding with ObserverPattern

--- index.js
+++ index.js
@@ -3,17 +3,23 @@ window.LC.Store = window.LC.Store || {
     Username: '',
     Password: '',
 };
-
-window.LC.ProxyStore = new Proxy(window.LC.Store, {
+window.LC.StoreObserve = new Set();
+window.LC.StoreProxy = new Proxy(window.LC.Store, {
     set(target, key, value, receiver) {
-        const result = Reflect.set(target, key, value, receiver);
-        document.querySelectorAll(`[data-bind=${key}]`).forEach(e => {
-            e.value = value
+        const result = Reflect.set(target, key, value, receiver)
+        Array.from(window.LC.StoreObserve).filter(a => a.key === key).forEach(observe => {
+            observe.element.value = value
         })
-        return result;
+        return result
     },
 })
 
 document.addEventListener('DOMContentLoaded', () => {
-
+    document.querySelectorAll(`[data-bind]`).forEach(e => {
+        let key = e.dataset.bind
+        window.LC.StoreObserve.add({
+            key: key,
+            element: e,
+        })
+    })
 });
