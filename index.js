d3502fa89614161ef059dc4745c302dc70a0c700
黃敏睿 <rayhuang@linkchain.tw>
2022-12-09 10:27:51 +0800

    feat: Support nested object value

--- index.js
+++ index.js
@@ -2,17 +2,29 @@ window.LC = window.LC || {};
 window.LC.Store = window.LC.Store || {
     Username: '',
     Password: '',
+    Info: {
+        Tel: '',
+        Email: '',
+    }
 };
 window.LC.StoreObserve = new Set();
 window.LC.StoreProxy = new Proxy(window.LC.Store, {
     set(target, key, value, receiver) {
-        const result = Reflect.set(target, key, value, receiver)
+        const result = ReflectNestedSet(target, key.split('.'), value)
         Array.from(window.LC.StoreObserve).filter(a => a.key === key).forEach(observe => {
-            observe.element.value = observe.transform(value)
+            if (observe.transform) { value = observe.transform(value) }
+            observe.element.value = value
         })
         return result
     },
 })
+function ReflectNestedSet(target, keys, value) {
+    if (keys.length === 1) {
+        Reflect.set(target, keys, value)        
+        return
+    }
+    return ReflectNestedSet(target[keys[0]], keys.slice(1), value)
+}
 
 window.LC.StoreTransform = {
     ToUpper: (text) => {
@@ -26,15 +38,21 @@ window.LC.StoreTransform = {
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll(`[data-bind]`).forEach(e => {
         let key = e.dataset.bind
-        let transform = e.dataset.transform
-        window.LC.StoreObserve.add({
+        let ObserveSetting = {
             key: key,
             element: e,
-            transform: window.LC.StoreTransform[transform]
-        })
+        }
+
+        let transform = e.dataset.transform
+        if (transform) {
+            ObserveSetting.transform = window.LC.StoreTransform[transform]
+        }
+
+        window.LC.StoreObserve.add(ObserveSetting)
+
 
         e.addEventListener('input', (event) => {
-            Reflect.set(window.LC.Store, key, event.currentTarget.value)
+            Reflect.set(window.LC.StoreProxy, key, event.currentTarget.value)
         })
     })
 });
